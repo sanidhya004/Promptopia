f23af21e577911bc251f645a83c39ce6ae7b6c7f
Sanidhya Sharma <sanidhya626@gmail.com>
2023-10-19 14:56:20 +0530

    v5

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,7 +2,7 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
-export const dynamic = 'force-dynamic';
+export const revalidate = 1; //revalidate api every 1 second
 export const GET = async (request) => {
     try {
         await connectToDB()
